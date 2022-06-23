34c8fcf0c19f40720856d064263c2d63ff0fdc84
MonroeLionel <MonroeLionel@gmail.com>
2022-06-24 00:01:38 +0300

    02 - JS/TS for students - объекты

---
+++ src/02/02.ts
@@ -0,0 +1,30 @@
+export type streetType = {
+    title: string
+}
+export type addressType = {
+    number: number
+    street: streetType
+}
+export type HousesType = {
+    buildedAt: number
+    repaired: boolean
+    address: addressType
+}
+export type governmentAddressStreetType = {
+    title: string
+}
+export type governmentAddressType = {
+    street: governmentAddressStreetType
+}
+export type governmentBuildingsTpe = {
+    type: `HOSPITAL` | `FIRE-STATION`
+    budget: number
+    staffCount: number
+    address: governmentAddressType
+}
+export type CityType = {
+    title: string
+    houses: Array<HousesType>
+    governmentBuildings: Array<governmentBuildingsTpe>
+    citizensNumber: number
+}
